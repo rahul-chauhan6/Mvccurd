5c7ba91b54a6bb2639eb3da57ffdedd3b73761d6
rahul-chauhan6 <78337923+rahul-chauhan6@users.noreply.github.com>
2022-03-17 10:54:24 +0530

    updated mvc project

---
+++ Controllers/EmployeeController.cs
@@ -0,0 +1,45 @@
+﻿using MVCCURD.dbcontent;
+using MVCCURD.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVCCURD.Controllers
+{
+    public class EmployeeController : Controller
+    {
+        // GET: Employee
+        [HttpGet]
+        public ActionResult Employee()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Employee(empmodel obj)
+        {
+            EmployeedbEntities1 dbobj = new EmployeedbEntities1();
+            empdetail tb=new empdetail();
+            tb.id= obj.id;
+            tb.Name = obj.Name;
+            tb.Phone = obj.Phone;
+            tb.salary = obj.salary;
+            dbobj.empdetails.Add(tb);
+            dbobj.SaveChanges();
+          
+
+            return RedirectToAction("Index","Home");
+        }
+
+        public ActionResult Delete(int id)
+        {
+            EmployeedbEntities1 dbobj1 = new EmployeedbEntities1();
+            var deleteItem = dbobj1.empdetails.Where(m => m.id == id).First();
+            dbobj1.empdetails.Remove(deleteItem);
+            dbobj1.SaveChanges();
+            return RedirectToAction("Index","Home");
+        }
+
+    }
+}
---
+++ Controllers/HomeController.cs
@@ -0,0 +1,54 @@
+﻿using MVCCURD.dbcontent;
+using MVCCURD.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVCCURD.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            EmployeedbEntities1 obj = new EmployeedbEntities1();
+            List<empmodel> li = new List<empmodel>();
+            var res=obj.empdetails.ToList();
+            foreach(var item in res)
+            {
+                li.Add(new empmodel
+                    {
+                    id = item.id,
+                    Name=item.Name,
+                    Phone=item.Phone,
+                    salary=item.salary
+                });
+            }
+          
+
+            return View(li);
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
+
+namespace MVCCURD
+{
+    class Model
+    {
+    }
+}
